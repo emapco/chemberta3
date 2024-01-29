b9d09c0cb34d0025f8b94e1812c4824a3d2562dc
arunppsg <arunppsg@gmail.com>
2024-01-29 23:28:12 +0530

    defined load_nek before usage

--- chemberta3/benchmarking/prepare_data.py
+++ chemberta3/benchmarking/prepare_data.py
@@ -33,6 +33,59 @@ FEATURIZER_MAPPING = {
     dc.feat.SNAPFeaturizer(),
 }
 
+
+def load_nek(
+    featurizer: dc.feat.Featurizer,
+    tasks_wanted: List[str] = ["NEK2_ki_avg_value"],
+    splitter=None,
+) -> Tuple[List[str], Tuple[dc.data.Dataset, ...], List[dc.trans.Transformer],
+           str]:
+    """Load NEK dataset.
+
+    The NEK dataset is a collection of datapoints related to the NEK kinases,
+    including SMILES strings, ki, kd, inhibition, and ic50 values.
+
+    NEK2_ki_avg_value contains the most non-nan data points. For other tasks,
+    load the NEK .csv file directly from "s3://chemberta3/datasets/kinases/NEK/nek_mtss.csv".
+
+    Mimics the loaders for molnet datasets.
+
+    Parameters
+    ----------
+    featurizer: dc.feat.Featurizer
+        Featurizer to use.
+    tasks_wanted: List[str]
+        Tasks to load. These should correspond to the columns in the dataframe.
+    splitter: dc.splits.splitters.Splitter
+        Splitter to use. This should be None, and is included only for compatibility.
+
+    Returns
+    -------
+    tasks: List[str]
+        List of tasks.
+    datasets: Tuple[dc.data.Dataset, ...]
+        Tuple of train, valid, test datasets.
+    transformers: List[dc.trans.Transformer]
+        List of transformers.
+
+    """
+    assert (
+        splitter is None
+    ), "Splitter arg only used for compatibility with other dataset loaders."
+    nek_df = pd.read_csv("s3://chemberta3/datasets/kinases/NEK/nek_mtss.csv",
+                         index_col=0)
+
+    with dc.utils.UniversalNamedTemporaryFile(mode="w") as tmpfile:
+        data_df = nek_df.dropna(subset=tasks_wanted)
+        data_df.to_csv(tmpfile.name)
+        loader = dc.data.CSVLoader(tasks_wanted,
+                                   feature_field="raw_smiles",
+                                   featurizer=featurizer)
+        dc_dataset = loader.create_dataset(tmpfile.name)
+
+    return [], [dc_dataset], []
+
+
 DATASET_MAPPING = {
     'delaney': dc.molnet.load_delaney,
     'bace_c': dc.molnet.load_bace_classification,
@@ -273,58 +326,6 @@ def prepare_data(dataset_name,
             # FIXME currently not supporting vocabulary preparation in multi-cpu featurization
 
 
-def load_nek(
-    featurizer: dc.feat.Featurizer,
-    tasks_wanted: List[str] = ["NEK2_ki_avg_value"],
-    splitter=None,
-) -> Tuple[List[str], Tuple[dc.data.Dataset, ...], List[dc.trans.Transformer],
-           str]:
-    """Load NEK dataset.
-
-    The NEK dataset is a collection of datapoints related to the NEK kinases,
-    including SMILES strings, ki, kd, inhibition, and ic50 values.
-
-    NEK2_ki_avg_value contains the most non-nan data points. For other tasks,
-    load the NEK .csv file directly from "s3://chemberta3/datasets/kinases/NEK/nek_mtss.csv".
-
-    Mimics the loaders for molnet datasets.
-
-    Parameters
-    ----------
-    featurizer: dc.feat.Featurizer
-        Featurizer to use.
-    tasks_wanted: List[str]
-        Tasks to load. These should correspond to the columns in the dataframe.
-    splitter: dc.splits.splitters.Splitter
-        Splitter to use. This should be None, and is included only for compatibility.
-
-    Returns
-    -------
-    tasks: List[str]
-        List of tasks.
-    datasets: Tuple[dc.data.Dataset, ...]
-        Tuple of train, valid, test datasets.
-    transformers: List[dc.trans.Transformer]
-        List of transformers.
-
-    """
-    assert (
-        splitter is None
-    ), "Splitter arg only used for compatibility with other dataset loaders."
-    nek_df = pd.read_csv("s3://chemberta3/datasets/kinases/NEK/nek_mtss.csv",
-                         index_col=0)
-
-    with dc.utils.UniversalNamedTemporaryFile(mode="w") as tmpfile:
-        data_df = nek_df.dropna(subset=tasks_wanted)
-        data_df.to_csv(tmpfile.name)
-        loader = dc.data.CSVLoader(tasks_wanted,
-                                   feature_field="raw_smiles",
-                                   featurizer=featurizer)
-        dc_dataset = loader.create_dataset(tmpfile.name)
-
-    return [], [dc_dataset], []
-
-
 def load_zinc5k(featurizer, data_dir: Optional[str] = None) -> None:
     """Featurizes saves zinc5k dataset in `data_dir`
 
@@ -420,10 +421,6 @@ def prepare_vocab(dataset_name, data_dir, split_dataset):
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
-    argparser.add_argument('--prepare_data',
-                           help='prepare data',
-                           default=False,
-                           action='store_true')
     argparser.add_argument('--multicpu_feat',
                            default=False,
                            action='store_true',
