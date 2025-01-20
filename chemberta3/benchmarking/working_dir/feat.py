9ed89982532dd2a73439ae6ef0c1c3d81c8674dd
Riya Singh <riyasingh3909@gmail.com>
2025-01-20 22:52:59 +0530

    update ray feat and train

--- chemberta3/benchmarking/working_dir/feat.py
+++ chemberta3/benchmarking/working_dir/feat.py
@@ -24,14 +24,23 @@ FEATURIZER_MAPPING = {
 
 def get_paths_from_args(args):
     result_path = 's3://chemberta3/featurized_data/' + args.dataset + '/' + args.featurizer
-    if args.dataset == 'zinc100m':
+    large_datasets = ['zinc100m', 'zinc250m', 'zinc500m', 'zinc1b']
+    if args.dataset in large_datasets:
+        if args.dataset == 'zinc100m':
+            upper_limit = 10
+        if args.dataset == 'zinc250m':
+            upper_limit = 25
+        if args.dataset == 'zinc500m':
+            upper_limit = 50
+        if args.dataset == 'zinc1b':
+            upper_limit = 100
         chunks = []
-        for i in range(0, 10):
+        for i in range(0, upper_limit):
             chunks.append('chunk_00' + str(i) + '.csv')
         csv_paths = []
         for chunk_path in chunks:
             csv_paths.append('s3://chemberta3/datasets/zinc20/csv/' +
-                             chunk_path)
+                            chunk_path)
         return csv_paths, result_path
     else:
         csv_path = 's3://chemberta3/datasets/' + args.dataset + '.csv'
@@ -48,10 +57,13 @@ def test_is_empty_path(path):
     count = bucket.objects.filter(Prefix=folder_name)
     assert len(list(count)) == 0, f'Result path is not empty {path}'
 
+
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--dataset', type=str, default='zinc250k')
     argparser.add_argument('--featurizer', type=str, default='dummy')
+    argparser.add_argument('--column', type=str, default='smiles')
+    argparser.add_argument('--label-column', type=str, default=None)
     args = argparser.parse_args()
 
     if args.dataset not in ['zinc250k', 'zinc1m', 'zinc10m', 'zinc100m']:
@@ -60,9 +72,17 @@ if __name__ == '__main__':
     test_is_empty_path(result_path)
 
     featurizer = FEATURIZER_MAPPING[args.featurizer]
+    ray.data.DataContext.get_current().execution_options.verbose_progress = True
+    large_datasets = ['zinc100m', 'zinc250m', 'zinc500m', 'zinc1b']
 
+    # Specifies the level of parallelism for the operation. Ray will process the CSV file in 100 parallel tasks, 
+    # which can improve loading speed, especially for large files.
     ds = ray.data.read_csv(csv_path, parallelism=100)
-    if args.dataset == 'zinc100m':
+
+    # Redistributes the dataset into 10,000 partitions in case of large datasets (>100M). This is typically 
+    # done to balance the data more evenly across computational resources or to adjust the partition size 
+    # for subsequent processing.
+    if args.dataset in large_datasets:
         ds = ds.repartition(10_000)
     ds = RayDataset(ds)
 
--- chemberta3/benchmarking/working_dir/main.py
+++ chemberta3/benchmarking/working_dir/main.py
@@ -9,30 +9,151 @@ from ray import train
 from data_utils import RayDataset
 import ray.train.torch
 from ray.train.torch import TorchTrainer
-from ray.train import Checkpoint, CheckpointConfig, RunConfig, ScalingConfig
-
+from ray.train import Checkpoint, CheckpointConfig, RunConfig, ScalingConfig, FailureConfig
 from transformers.models.roberta.tokenization_roberta_fast import RobertaTokenizerFast
 from transformers.data.data_collator import DataCollatorForLanguageModeling
 
 
-def train_loop_per_worker(config):
+LR_SCHEDULE_MAPPINGS = {
+    'polynomial_decay': {
+        "scheduler_class": dc.models.optimizers.PolynomialDecay,
+        "init_params": {
+            "required_init_params": [
+                "initial_rate", "final_rate", "decay_steps"
+            ],
+            "optional_init_params": ["power"]
+        }
+    },
+    'exponential_decay': {
+        "scheduler_class": dc.models.optimizers.ExponentialDecay,
+        "init_params": {
+            "required": ["initial_rate", "decay_rate", "decay_steps"],
+            "optional": ["staircase"]
+        }
+    },
+    'linear_cosine_decay': {
+        "scheduler_class": dc.models.optimizers.LinearCosineDecay,
+        "init_params": {
+            "required": ["initial_rate", "decay_steps"],
+            "optional": ["alpha", "beta", "num_periods"]
+        }
+    },
+    'piecewise_constant_schedule': {
+        "scheduler_class": dc.models.optimizers.PiecewiseConstantSchedule,
+        "init_params": {
+            "required": ["initial_rate"],
+            "optional": ["boundaries_and_scales"]
+        }
+    },
+    'lambda_lr_with_warmup': {
+        "scheduler_class": dc.models.optimizers.LambdaLRWithWarmup,
+        "init_params": {
+            "required": [
+                "initial_rate", "num_warmup_steps", "num_training_steps"
+            ],
+            "optional": ["warmup_type"]
+        }
+    },
+}
+
+
+MODEL_MAPPINGS = {}
+MODEL_MAPPINGS['chemberta'] = dc.models.torch_models.chemberta.Chemberta
+MODEL_MAPPINGS['grover'] = dc.models.torch_models.GroverModel
+MODEL_MAPPINGS['molformer'] = dc.models.torch_models.MoLFormer
+
+LOSS_ARG_NAME = 'iteration_loss'
+
+
+def _callback(model, step, iteration_loss, epoch):
+    """Callback to pass during model.fit call.
+
+    Parameters
+    ----------
+    model: dc.models.Model
+        A DeepChem model
+    loss: float
+        Loss in an iteration
+    step: int
+        Training iteration
+
+    NOTE: Callback is important as the ray.train.report method ensures synchronization
+    of gradients across workers. Otherwise, training will hang.
+    """
+    world_rank = ray.train.get_context().get_world_rank()
+    loss = iteration_loss.detach().cpu().numpy().item()
+    metrics = {'iteration': step, LOSS_ARG_NAME: loss}
+    with tempfile.TemporaryDirectory() as temp_checkpoint_dir:
+        checkpoint = None
+        if world_rank == 0 and (step % model.checkpoint_frequency == 0 or
+                                step == model.total_steps):
+            model_dir = os.path.join(temp_checkpoint_dir, 'model_ckpt')
+            data_path = os.path.join(temp_checkpoint_dir, 'data.pt')
+            os.makedirs(model_dir)
+            data = {'epoch': epoch}
+
+            model.save_checkpoint(model_dir=model_dir)
+            torch.save(data, data_path)
+
+            checkpoint = Checkpoint.from_directory(temp_checkpoint_dir)
+        ray.train.report(metrics, checkpoint=checkpoint)
+
+
+def _train_loop_per_worker(config):
+    """Ray training loop"""
     device = ray.train.torch.get_device()
-    dc_model = dc.models.torch_models.Chemberta(task='mlm',
-                                                learning_rate=0.0001)
-    batch_size = 16
+    model_name = config['model_name']
 
-    model = ray.train.torch.prepare_model(dc_model.model)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    # The `"learning_rate"` parameter in `init_kwargs` can either be the name of a Learning Rate Scheduler or a float value.
+    # The available LR Scheduler types are `'polynomial_decay'`, `'exponential_decay'`, `'linear_cosine_decay'`, and `'piecewise_constant_schedule'`.
+    # If `"learning_rate"` is a string, the system checks whether it corresponds to a valid scheduler type in the mapping and verifies that all required parameters for the scheduler class are present in `init_kwargs`.
+    # It then initializes the scheduler and passes it as the "learning_rate" parameter to the model.
+    init_kwargs = config['init_kwargs']
+    if 'learning_rate' in init_kwargs.keys():
+        if isinstance(init_kwargs['learning_rate'], str):
+            lr_scheduler = init_kwargs['learning_rate']
+            if lr_scheduler not in LR_SCHEDULE_MAPPINGS:
+                raise ValueError("Learning rate scheduler type not recognized.")
+            lr_scheduler_params = {}
+            lr_scheduler_params_dict = LR_SCHEDULE_MAPPINGS[lr_scheduler][
+                'init_params']
+            for type, args in lr_scheduler_params_dict.items():
+                for arg in args:
+                    if str(arg) in init_kwargs:
+                        lr_scheduler_params[arg] = init_kwargs[arg]
+                    else:
+                        if type == 'required':
+                            raise Exception(
+                                f"Missing required parameters for Learning Rate Scheduler: {lr_scheduler}"
+                            )
+            print(lr_scheduler_params)
+            learning_rate_scheduler = LR_SCHEDULE_MAPPINGS[lr_scheduler][
+                'scheduler_class'](**lr_scheduler_params)
 
-    train_data_shard = train.get_dataset_shard("train")
-    train_dataloader = train_data_shard.iter_batches(batch_size=batch_size)
+        elif isinstance(init_kwargs['learning_rate'], float):
+            learning_rate_scheduler = init_kwargs['learning_rate']
 
-    # Chemberta-MLM specific lines
-    tokenizer_path = 'seyonec/PubChem10M_SMILES_BPE_60k'
-    tokenizer = RobertaTokenizerFast.from_pretrained(tokenizer_path)
-    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer)
+        init_kwargs['learning_rate'] = learning_rate_scheduler
+        init_kwargs['optimizer'] = dc.models.optimizers.Lamb(learning_rate=learning_rate_scheduler)
 
+    total_steps = init_kwargs['total_steps']
+    del init_kwargs['total_steps']
+    
+    # `checkpoint_frequency` determines how often checkpoints are created during training. By default it is set to 100 iterations.
+    if 'checkpoint_frequency' in init_kwargs:
+        checkpoint_frequency = init_kwargs['checkpoint_frequency']
+        del init_kwargs['checkpoint_frequency']
+    else:
+        checkpoint_frequency = 100
+    dc_model = MODEL_MAPPINGS[model_name](**init_kwargs)
+    dc_model.checkpoint_frequency = checkpoint_frequency
+    dc_model.total_steps = total_steps
     checkpoint = train.get_checkpoint()
+
+    # model should be prepared for the distributed format before restoring the checkpoint
+    # otherwise the model restore fails with error that no params found with `module.` prefix.
+    dc_model.model = ray.train.torch.prepare_model(dc_model.model)
+    dc_model.device = device
     if checkpoint:
         print('checkpoint is present')
         with checkpoint.as_directory() as ckpt_dir:
@@ -47,41 +168,12 @@ def train_loop_per_worker(config):
         print('checkpoint is not present')
         start_epoch = 0
 
-    # TODO Compile the model here before training for speedup
+    train_dataset = RayDataset(train.get_dataset_shard("train"))
+    train_dataloader = train_data_shard.iter_batches(batch_size=config["batch_size"])
+
     for epoch in range(start_epoch, config['num_epochs']):
-        for i, batch in enumerate(train_dataloader):
-            optimizer.zero_grad()
-            optimizer.zero_grad()
-            inputs = batch['smiles']
-            tokens = tokenizer(inputs.tolist(),
-                               padding=True,
-                               return_tensors="pt")
-            inputs, labels = data_collator.torch_mask_tokens(
-                tokens['input_ids'])
-            inputs = {
-                'input_ids': inputs.to(device),
-                'labels': labels.to(device),
-                'attention_mask': tokens['attention_mask'].to(device),
-            }
-            outputs = model(**inputs)
-            loss = outputs.get("loss")
-            loss.backward()
-            optimizer.step()
-            loss = loss.detach().cpu().item()
-
-            metrics = {"loss": loss, "epoch": epoch, "iteration": i}
-            with tempfile.TemporaryDirectory() as temp_checkpoint_dir:
-                data = {
-                    'model': model.state_dict(),
-                    'optimizer': optimizer.state_dict(),
-                    'epoch': epoch,
-                    'loss': loss
-                }
-                torch.save(data, "ckpt.pt")
-                ray.train.report(
-                    metrics,
-                    checkpoint=Checkpoint.from_directory(temp_checkpoint_dir))
-
+        callback = partial(_callback, epoch=epoch)
+        dc_model.fit(train_dataset, nb_epoch=1, callbacks=[callback])
 
 if __name__ == '__main__':
     with open('config.yaml', 'r'):
@@ -94,12 +186,21 @@ if __name__ == '__main__':
 
     # dataset_path = train_data_dir
     # train_dataset = RayDataset.read(dataset_path).dataset
-    train_loop_config = {"num_epochs": config['num_epochs']}
+    train_loop_config = {"num_epochs": config['num_epochs'], "model_name": model_name, "init_kwargs": config['init_kwargs']}
     storage_path, exp_name = config['storage_path'], config['exp_name']
+
+    # `num_to_keep` parameter specifies that a maximum of 5 checkpoints will be retained at any given time. Older checkpoints will be 
+    # discarded as new ones are created, ensuring storage efficiency. `checkpoint_score_order`=`min` indicates that the checkpoint with 
+    # the smallest loss will be preferred.
     ckpt_config = CheckpointConfig(num_to_keep=5,
-                                   checkpoint_score_attribute='loss',
+                                   checkpoint_score_attribute=LOSS_ARG_NAME,
                                    checkpoint_score_order='min')
+
+    # In a distributed system, tasks may fail intermittently due to network issues, resource constraints, or other transient errors. 
+    # This setting allows the system to tolerate a specific number of such failures before considering the process unsuccessful.
+    failure_config = FailureConfig(max_failures=200)                               
     run_config = RunConfig(checkpoint_config=ckpt_config,
+                           failure_config=failure_config,
                            name=exp_name,
                            storage_path=storage_path)
     exp_path = os.path.join(storage_path, exp_name)
