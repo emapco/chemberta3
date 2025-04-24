263dbe828206ac870da5835b49d804a0552c9254
Riya Singh <riyasingh3909@gmail.com>
2025-04-24 22:39:10 +0530

    add regression and classification scripts for chemberta benchmarking"

---
+++ chemberta3_benchmarking/models_benchmarking/chemberta_benchmark/chemberta_finetune_classification_script.sh
@@ -0,0 +1,7 @@
+python3 chemberta_finetune_classification.py\
+        --datasets "bace,bbbp,tox21,hiv,sider,clintox" \
+        --batch_size 128  \
+        --epochs 100 \
+        --pretrained_model_path '../../data/pretrained_model_checkpoints/pretrained_chemberta/chemberta-100M-mlm-4epochs' \
+        --splits_name 'molformer_splits' \
+        --learning_rate 3e-5 \
---
+++ chemberta3_benchmarking/models_benchmarking/chemberta_benchmark/chemberta_finetune_regression.py
@@ -0,0 +1,185 @@
+import os
+import torch
+import args
+import logging
+import numpy as np
+import deepchem as dc
+from deepchem.models.torch_models import Chemberta
+import random
+from datetime import datetime
+import shutil
+
+
+# Set seeds
+def set_seed(seed):
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+
+def setup_logging(dataset, epochs, batch_size):
+    log_dir = 'logs_chemberta'
+    os.makedirs(log_dir, exist_ok=True)
+    datetime_str = datetime.now().strftime("%Y%m%d_%H%M%S")
+    log_path = os.path.join(log_dir, f"chemberta_molformer_splits_run_{dataset}_epochs{epochs}_batch_size{batch_size}_{datetime_str}.log")
+
+    logger = logging.getLogger(f"logs_chemberta_{dataset}_epochs{epochs}_batch_size{batch_size}")
+    logger.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler(log_path)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    
+    # Avoid adding multiple handlers to the logger
+    if not logger.handlers:
+        logger.addHandler(file_handler)
+    
+    return logger
+    
+
+def model_fn(tasks, model_dir, batch_size, learning_rate, pretrained_model_path): 
+    # training model
+    
+    finetune_model = Chemberta(task='regression', learning_rate=learning_rate, batch_size=batch_size, n_tasks=len(tasks), model_dir=model_dir)
+    finetune_model.load_from_pretrained(pretrained_model_path)
+    return finetune_model
+
+
+def run_deepchem_experiment(run_id, 
+                            model_fn, 
+                            train_dataset, 
+                            valid_dataset, 
+                            test_dataset, 
+                            metric, 
+                            dataset, 
+                            tasks, 
+                            model_dir, 
+                            batch_size,
+                            learning_rate, 
+                            pretrained_model_path, 
+                            epochs=50, 
+                            logger=None):
+    
+    set_seed(run_id)
+    model = model_fn(tasks, model_dir, batch_size, learning_rate, pretrained_model_path)
+    best_score = np.inf
+
+    # Get current datetime and format it
+    current_datetime = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    best_model_dir = f"checkpoints_{dataset}/best_model_epochs{epochs}_run{run_id}_{current_datetime}"
+    os.makedirs(f"checkpoints_{dataset}", exist_ok=True)
+
+    for epoch in range(epochs):
+
+        loss = model.fit(train_dataset, nb_epoch=1, restore=epoch>0, max_checkpoints_to_keep=1)
+        scores = model.evaluate(valid_dataset, [metric])
+        val_score = scores[metric.name]
+
+        logger.info(f"Epoch {epoch+1}/{epochs} | Train Loss: {loss:.4f} | Val {metric.name}: {val_score:.4f}")
+
+        if val_score < best_score:
+            best_score = val_score
+            # model.save_checkpoint(max_checkpoints_to_keep=1)
+
+            # Save current checkpoint as best model
+            if os.path.exists(best_model_dir):
+                shutil.rmtree(best_model_dir)
+            logger.info(f"Global step of best model: {model._global_step}")
+            shutil.copytree(model.model_dir, best_model_dir)
+
+            logger.info(f"Best model saved at epoch {epoch+1} with val {metric.name}: {val_score:.4f}")
+
+    # Load best checkpoint before evaluating on test set
+    model.restore(model_dir=best_model_dir)
+    test_scores = model.evaluate(test_dataset, [metric])
+    test_score = test_scores[metric.name]
+    logger.info(f"🏁 Test {metric.name}: {test_score:.4f}")
+
+    return test_score
+
+
+def triplicate_benchmark_dc(dataset,
+                            splits_name,
+                            model_fn, 
+                            metric, 
+                            tasks, 
+                            batch_size,
+                            learning_rate, 
+                            pretrained_model_path, 
+                            nb_epoch=50, 
+                            logger=None):
+    scores = []
+    train_dataset = dc.data.DiskDataset(f'../finetune_data/{splits_name}/dummy_featurized/{dataset}/train')
+    valid_dataset = dc.data.DiskDataset(f'../finetune_data/{splits_name}/dummy_featurized/{dataset}/valid')
+    test_dataset = dc.data.DiskDataset(f'../finetune_data/{splits_name}/dummy_featurized/{dataset}/test')
+
+    for run_id in range(3):
+        current_datetime = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        logger.info(f"Starting triplicate run {run_id + 1} for dataset {dataset} at {current_datetime}")
+        model_dir = f'chemberta_model_dir_{dataset}_{run_id}_{current_datetime}'
+        test_score = run_deepchem_experiment(
+            run_id, model_fn,
+            train_dataset, valid_dataset, test_dataset,
+            metric, dataset=dataset, tasks=tasks, model_dir=model_dir,
+            batch_size=batch_size, learning_rate=learning_rate,
+            pretrained_model_path=pretrained_model_path, epochs=nb_epoch,
+            logger=logger
+        )
+        scores.append(test_score)
+
+    avg_score = np.mean(scores)
+    std_score = np.std(scores)
+    logger.info(f"Final Triplicate Test Results — Avg {metric.name}: {avg_score:.4f}, Std Dev: {std_score:.4f}")
+    return avg_score, std_score
+
+
+def main():
+    margs = args.parse_args()
+    datasets = margs.datasets
+    datasets = args.dataset.split(',')
+    learning_rate = margs.learning_rate
+    splits_name = margs.splits_name
+
+    if datasets is None:
+        raise ValueError("Please provide a list of datasets to benchmark.")
+    if not isinstance(datasets, list):
+        raise ValueError("Datasets should be provided as a list.")
+    if len(datasets) == 0:
+        raise ValueError("The list of datasets is empty. Please provide at least one dataset.")
+    epochs = margs.epochs
+    batch_size = margs.batch_size
+    pretrained_model_path = margs.pretrained_model_path
+    if pretrained_model_path is None:
+        raise ValueError("Please provide a path to the pretrained model.")
+    if not os.path.exists(pretrained_model_path):
+        raise ValueError(f"Pretrained model path {pretrained_model_path} does not exist.")
+
+    task_dict = {'esol': ['measured_log_solubility_in_mols_per_litre'],
+                'freesolv': ['expt'],
+                'lipo': ['y'],
+                }
+
+    metric = dc.metrics.Metric(dc.metrics.rms_score)
+    regression_datasets = datasets
+    for dataset in regression_datasets:
+        if dataset not in task_dict:
+            raise ValueError(f"Dataset {dataset} not found in task_dict.")
+        logger = setup_logging(dataset=dataset, epochs=epochs, batch_size=batch_size)
+        logger.info(f"Running benchmark for dataset: {dataset}")
+
+        tasks = task_dict[dataset]
+        logger.info(f"dataset: {dataset}, tasks: {tasks}, epochs: {epochs}")
+        triplicate_benchmark_dc(dataset=dataset,
+                                splits_name=splits_name,
+                                model_fn=model_fn, 
+                                metric=metric, 
+                                tasks=tasks, 
+                                batch_size=batch_size,
+                                learning_rate=learning_rate, 
+                                pretrained_model_path=pretrained_model_path, 
+                                nb_epoch=epochs, 
+                                logger=logger)
+
+
+if __name__ == "__main__":
+    main()
