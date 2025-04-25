30696bc1ea25f2e44b483e014baaf778b198b28b
Riya Singh <riyasingh3909@gmail.com>
2025-04-25 21:57:01 +0530

    add docstrings and readme

--- chemberta3_benchmarking/models_benchmarking/chemberta_benchmark/chemberta_finetune_classification_script.sh
+++ chemberta3_benchmarking/models_benchmarking/chemberta_benchmark/chemberta_finetune_classification_script.sh
@@ -1,6 +1,6 @@
 python3 chemberta_finetune_classification.py\
         --datasets "bace,bbbp,tox21,hiv,sider,clintox" \
-        --batch_size 128  \
+        --batch_size 32  \
         --epochs 100 \
         --pretrained_model_path '../../data/pretrained_model_checkpoints/pretrained_chemberta/chemberta-100M-mlm-4epochs' \
         --splits_name 'molformer_splits' \
