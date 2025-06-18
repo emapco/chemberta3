6ae79162baefd6e639556a389d8495c742d37eb4
Riya Singh <riyasingh3909@gmail.com>
2025-06-18 22:45:30 +0530

    add grover finetuning scripts

---
+++ chemberta3_benchmarking/models_benchmarking/grover_benchmark/grover_regression_benchmark_script.sh
@@ -0,0 +1,16 @@
+python3 grover_regression_benchmark.py\
+        --datasets "freesolv,bace_regression,clearance,esol" \
+        --splits_name 'deepchem_splits' \
+        --transform True \
+        --batch_size 100 \
+        --vocab_data_path '../../data/dummy_featurized/zinc250k_dummy_featurized' \
+        --node_fdim 151 \
+        --edge_fdim 165 \
+        --feature_dim 2048 \
+        --hidden_size 128 \
+        --functional_group_size 85 \
+        --learning_rate 3e-5 \
+        --epochs 100 \
+        --pretrained_model_path '' \
+
+# please provide the pretrained_model_path
